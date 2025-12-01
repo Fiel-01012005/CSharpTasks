1855675f637a3cba8e1fce2e3e9e2496692e1e87
Kenchin Fiel Reducto <kenchinfielmreducto@gmail.com>
2025-12-01 00:07:29 +0000

    Tasks commit message for submission

---
+++ ConsoleApp1/Task3.cs
@@ -0,0 +1,32 @@
+namespace ConsoleApp1;
+public class Task3
+{
+    static void Main(string[] args)
+{
+        int[] numbers = { 3, 7, 12, 19, 21, 25, 30 };
+
+        // Asking the User to enter a number to search
+        Console.Write("Enter a number to search for: ");
+        int key = int.Parse(Console.ReadLine());
+
+        bool found = false;
+
+        //  Using For Loop to find the entered number
+        for (int i = 0; i < numbers.Length; i++)
+        {
+
+            // Breaking the loop if the entered number is found
+            if (numbers[i] == key)
+            {
+                Console.WriteLine($"Element found at position {i + 1}.");
+                found = true;
+                break;
+            }
+        }
+
+        if (!found)
+        {
+            Console.WriteLine("Element not found.");
+        }
+    }
+}
